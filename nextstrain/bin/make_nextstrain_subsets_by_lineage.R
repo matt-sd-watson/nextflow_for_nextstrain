c313072bf470f6aec633b15b6738e86b96c641b4
Matthew Watson <matthew.watson@oahpp.ca>
2021-07-19 16:25:07 -0400

    Use optparse for lineage workflow (R)

--- nextstrain/bin/make_nextstrain_subsets_by_lineage.R
+++ nextstrain/bin/make_nextstrain_subsets_by_lineage.R
@@ -1,31 +1,31 @@
-library(argparse)
-library(dplyr)
 library(data.table)
+library(dplyr)
+library(optparse)
 
-parser <- ArgumentParser(description='Create subsets by lineage for a Nextstrain build')
-parser$add_argument('--input_lineage', type = "character", 
-                    help='Input lineage report CSV for PHO sequences')
-parser$add_argument('--lineage_id', type = "character", 
-                    help='ID of the lineage to create subset')
-parser$add_argument('--input_metadata', type = "character", 
-                    help='ID of the lineage to create subset')
 
-# parser$add_argument('--output_dir', type = "character", 
-                    # help='target output dir')
+option_list = list(
+  make_option(c("-i", "--input_lineage"), type="character", default=NULL, 
+              help="Input lineage report CSV for PHO sequences", metavar="character"),
+  make_option(c("-l", "--lineage_id"), type="character", default=NULL, 
+              help="ID of the lineage to create subset", metavar="character"),
+  make_option(c("-m", "--input_metadata"), type="character", default=NULL, 
+              help="path to the master metadata sheet for Nextstrain", metavar="character")
+); 
 
-args <- parser$parse_args()
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
 # read the pangolin lineage report and get the WGS Ids with the lineage
-pangolin_pho <- read.table(args$input_lineage, header = TRUE, sep=',', fill = TRUE, quote = "")
+pangolin_pho <- read.table(opt$input_lineage, header = TRUE, sep=',', fill = TRUE, quote = "")
 
-subset_pho <- subset(pangolin_pho, lineage == args$lineage_id)
+subset_pho <- subset(pangolin_pho, lineage == opt$lineage_id)
 
 # write the unique IDs only
 unique_ids <- unique(subset_pho$taxon)
 
 # read in metadata from the qc90 list
 pho_data <- read.table(
-  args$input_metadata,
+  opt$input_metadata,
   header = T,
   sep = ',',
   fill = TRUE,
@@ -37,7 +37,7 @@ pho_data_subset_lineage <- pho_data[pho_data$WGS_Id %in% unique_ids,]
 setnames(pho_data_subset_lineage, "PHO.WGS.Id", "strain")
 setnames(pho_data_subset_lineage, "Date", "date")
 
-output_file <- paste(args$lineage_id, ".csv", sep="") # replace with desired output directory
+output_file <- paste(opt$lineage_id, ".csv", sep="") # replace with desired output directory
 write.csv(pho_data_subset_lineage,
           output_file,
           row.names = F,
